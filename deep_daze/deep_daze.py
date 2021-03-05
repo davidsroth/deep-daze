00d3f55d74933286b7e60379bf63209b08bb3f4d
David <davidroth6@gmail.com>
2021-03-05 17:06:45 -0500

    fix image normalization

--- deep_daze/deep_daze.py
+++ deep_daze/deep_daze.py
@@ -41,7 +41,8 @@ def signal_handling(signum, frame):
 
 signal.signal(signal.SIGINT, signal_handling)
 
-perceptor, normalize_image = clip.load('RN101', jit=True)
+perceptor, preprocess = clip.load('RN101', jit=True)
+normalize_image = T.Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711))
 
 # Helpers
 
